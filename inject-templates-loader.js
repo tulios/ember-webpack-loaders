f3febf254975508a20a14e5e9973b5d0fd0f064c
Túlio Ornelas <tulio.ornelas@klarna.com>
2016-01-21 20:59:33 +0100

    inject templates loader

---
+++ inject-templates-loader.js
@@ -0,0 +1,34 @@
+var path = require('path')
+var loaderUtils = require('loader-utils')
+var recursive = require('recursive-readdir-sync')
+
+/*
+ * options:
+ *  - appPath: default assuming webpack.config.js in root folder + ./app
+ */
+module.exports = function(source) {
+  var options = loaderUtils.parseQuery(this.query);
+  var appPath = (options.appPath || 'app').replace(/\/$/, '')
+  appPath = path.resolve(appPath)
+  var templatesFolder = path.join(appPath, 'templates')
+
+  try {
+    var templates = []
+    var files = recursive(templatesFolder)
+    files.forEach(function(file) {
+      var templateRelativePath = file.replace(appPath, '.')
+      templates.push("import '" + templateRelativePath + "'")
+    })
+
+    source = source + '\n\n' + templates.join('\n')
+
+  } catch(err) {
+    if (err.errno === 34) {
+      console.log('Path ' + TEMPLATES_FOLDER + ' does not exist');
+    } else {
+      throw err;
+    }
+  }
+
+  return source
+}
